b3714fc02a80798f760e2d54bfff591671b6dac2
hhhannahmmmontana <rubakhin.edu@niuitmo.ru>
2026-01-16 19:51:26 +0300

    feat: basic compose

---
+++ init/init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE order_service_db;
+CREATE DATABASE user_service_db;
+CREATE DATABASE reservation_service_db;
+CREATE DATABASE inventory_service_db;
+CREATE DATABASE task_service_db;
+CREATE DATABASE banking_service_db;
+CREATE DATABASE statistics_service_db;
